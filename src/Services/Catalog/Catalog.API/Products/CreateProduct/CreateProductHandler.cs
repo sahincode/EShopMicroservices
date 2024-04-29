371965af5d76f80fff9336825485353e6063645d
sahinismayil <ismayilovsahin714@gmail.com>
2024-04-29 23:47:38 +0400

    BuildingBlocks solution folder added.

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile ,decimal price);
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler
+    {
+    }
+}
