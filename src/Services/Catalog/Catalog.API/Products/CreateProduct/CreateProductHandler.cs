cb32b1b612003825fa10e37f19e8c4860586bda7
sahinismayil <ismayilovsahin714@gmail.com>
2024-04-30 01:19:56 +0400

    CreateProductEndpoint class created.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,10 +1,28 @@
-﻿using System.Runtime.CompilerServices;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+using System.Runtime.CompilerServices;
 
 namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile ,decimal price);
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
+        public async  Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create Product entity from the command object
+            //save to database
+            //return CreateProductResult result
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            return new CreateProductResult(Guid.NewGuid());
+        }
     }
 }
