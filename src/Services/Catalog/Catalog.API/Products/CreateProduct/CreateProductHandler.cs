238f99e1e4164600581f9536279b8974f1020122
sahinismayil <ismayilovsahin714@gmail.com>
2024-05-11 16:26:46 +0400

    GetProductHandler added.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,6 @@
 ﻿using BuildingBlocks.CQRS;
 using Catalog.API.Models;
+using Marten;
 using MediatR;
 using System.Runtime.CompilerServices;
 
@@ -7,7 +8,7 @@ namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async  Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -22,6 +23,8 @@ namespace Catalog.API.Products.CreateProduct
                 ImageFile = command.ImageFile,
                 Price = command.Price
             };
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
             return new CreateProductResult(Guid.NewGuid());
         }
     }
---
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.GetProduct
+{
+   public record GetProductsResponse(IEnumerable<Product> Products);
+    public class GetProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductQuery());
+                var response = result.Adapt<GetProductsResponse>();
+                return Results.Ok(response);
+            }).WithName("GetProducts").
+            Produces<GetProductsResponse>(StatusCodes.Status200OK).
+            ProducesProblem(StatusCodes.Status400BadRequest).
+            WithSummary("Get Products").
+            WithDescription("Get Products");
+
+        }
+    }
+}
