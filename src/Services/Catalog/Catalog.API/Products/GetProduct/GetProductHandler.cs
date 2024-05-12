644da2a238d72d3fa74636a581a2176e78d40bae
sahinismayil <ismayilovsahin714@gmail.com>
2024-05-13 01:15:07 +0400

    GetProductById Endpoint and Handler added .

--- src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductHandler.cs
@@ -8,7 +8,7 @@ namespace Catalog.API.Products.GetProduct
     public record GetProductsResult(IEnumerable<Product> Products);
     internal class GetProductsQueryHandler(IDocumentSession session , ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductQuery, GetProductsResult>
     {
-        public async Task<GetProductsResult> Handle(GetProductQuery request, CancellationToken cancellationToken)
+        public async Task<GetProductsResult> Handle(GetProductQuery query, CancellationToken cancellationToken)
         {
             logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
             var products =await session.Query<Product>().ToListAsync(cancellationToken);
