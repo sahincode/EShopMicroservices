a165b6b1818e5b5b8bf9977a5df8d5a9fdee8594
sahinismayil <ismayilovsahin714@gmail.com>
2024-05-14 00:10:27 +0400

    Delete product handler and endpoint created.

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+    public class DeleteProductCommandHandler(IDocumentSession session ,ILogger<DeleteProductCommandHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductCommandHandler.Handler called with {@Command}", command);
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+            return new DeleteProductResult(true);
+
+        }
+    }
+}
