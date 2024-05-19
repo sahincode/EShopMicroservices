8c53c8e96aa30d837bb9a0fe2c53c37e14432553
sahincode <ismayilovsahin714@gmail.com>
2024-05-19 10:37:56 +0400

    Global exception handler traded  for  custom exception handler.

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -2,3 +2,5 @@
 global using Mapster;
 global using MediatR;
 global using FluentValidation;
+global using BuildingBlocks.Behaviours;
+global using BuildingBlocks.Exceptions.Handler;
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,6 @@
-using BuildingBlocks.Behaviours;
-using Carter;
+
+
 using Marten;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using OpenTelemetry.Trace;
-using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
@@ -20,31 +16,13 @@ builder.Services.AddMarten(opts =>
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 
 }).UseLightweightSessions();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 var app = builder.Build();
 
 app.MapCarter();
-app.UseExceptionHandler(exceptionHandlerApp =>
+app.UseExceptionHandler(options =>
 {
-    exceptionHandlerApp.Run(async context =>
-    {
-      var exception =context.Features.Get<IExceptionHandlerFeature>()?.Error;
-        if (exception == null)
-        {
-            return;
-        }
-        var problemDetails = new ProblemDetails()
-        {
-            Title = exception.Message,
-            Status = StatusCodes.Status500InternalServerError,
-            Detail = exception.StackTrace
-        };
-        var logger =context.RequestServices.GetRequiredService<ILogger<Program>>();
-        logger.LogError(exception, exception.Message);
-        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        context.Response.ContentType = "application/problem+json";
-        await context.Response.WriteAsJsonAsync(problemDetails);
-    });
-});
 
+});
 
 app.Run();
