738045c03ce7d253c0c3908edaf968b35607b23a
sahincode <ismayilovsahin714@gmail.com>
2024-10-20 22:19:36 +0400

    GetOrdersByName and GetOrdersByCustomer  in Ordering.API  layer created successfully.

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,11 +1,31 @@
-﻿namespace Ordering.API.Endpoints
+﻿
+using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.Endpoints
 {
     //- Accept the order ID as a parameter .
     //- Constract a DeleteOrderCommand.
     //- Sends the command using MediatR.
     //- Returns a success or not found response.
+    public record DeleteOrderRequest(Guid Id);
 
-    public class DeleteOrder
+    public record DeleteOrderResponse(bool ISSuccess);
+    public class DeleteOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+           app.MapDelete("/orders/{id}", async (Guid Id, ISender sender) =>
+           {
+               var result = await sender.Send(new DeleteOrderCommand(Id));
+               var response = result.Adapt<DeleteOrderResponse>();
+               return Results.Ok(response);
+
+           }).WithName("DeleteOrder")
+            .Produces<CreateOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Create Order")
+            .WithDescription("Create Order");
+        }
     }
 }
