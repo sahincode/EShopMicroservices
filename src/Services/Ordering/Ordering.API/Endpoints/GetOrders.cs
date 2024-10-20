6b9d098341a441f23b329a4f9cabe641fe213c0e
sahincode <ismayilovsahin714@gmail.com>
2024-10-21 01:22:56 +0400

    GetOrders  endpint in Ordering.API layer is  created successfully.

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,10 +1,30 @@
-﻿namespace Ordering.API.Endpoints
+﻿using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints
 {
     //- Accepts pagination paraemters.
     //- Constracts a GetOrdersQuery with these parameters.
     //- Retrieves the data and returns it in a paginated format.
+    //public record GetOrdersRequest(PaginationRequest PaginationRequest);
+    public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
 
-    public class GetOrders
+
+    public class GetOrders : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+           app.MapGet("/orders", async([AsParameters] PaginationRequest request, ISender sender) =>
+           {
+               var result = await sender.Send(new GetOrdersQuery(request));
+               var response = result.Adapt<GetOrdersQuery>();
+               return Results.Ok(response);
+           }).WithName("GetOrders")
+            .Produces<GetOrdersByNameResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Orders")
+            .WithDescription("Get Orders");
+        }
     }
 }
