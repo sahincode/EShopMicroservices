df4cb8312998702a598578056e7bc4932720c90f
sahincode <ismayilovsahin714@gmail.com>
2024-10-23 02:18:00 +0400

    UpdateOrder Endpoind is tested sucessfully.

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderUpdatedOrderHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderUpdatedOrderHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Application.Orders.Commands.UpdateOrder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Orders.EventHandlers
 {
-    internal class OrderUpdatedOrderHandler
+    internal class OrderUpdatedEventHandler(ILogger<OrderUpdatedEventHandler> logger) :INotificationHandler<OrderUpdateEvent>
     {
+        public Task Handle(OrderUpdateEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Domain Event handler :{DomainEvent}", notification.GetType());
+            return Task.CompletedTask;
+        }
     }
 }
+
